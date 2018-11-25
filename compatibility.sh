af910f1be72b6b887e6c04388700faf5d8b2a05b
Robert de Bock <robert@meinit.nl>
2018-11-25 14:11:54 +0100

    Reduce to 3 releases.

--- compatibility.sh
+++ compatibility.sh
@@ -2,8 +2,8 @@
 
 echo "This role has been tested against the following distributions and Ansible version:"
 echo ""
-echo "|distribution|ansible 2.4|ansible 2.5|ansible 2.6|ansible 2.7|ansible devel|"
-echo "|------------|-----------|-----------|-----------|-----------|-------------|"
+echo "|distribution|ansible 2.6|ansible 2.7|ansible devel|"
+echo "|------------|-----------|-----------|-------------|"
 cat .travis.yml | docker run -i --rm jlordiales/jyparser get ".env" | while read dash version distro rest ; do
   echo "${distro}" | cut -d\" -f2 | sort | uniq | while read distribution ; do
   case "${distribution}" in
@@ -20,12 +20,10 @@ cat .travis.yml | docker run -i --rm jlordiales/jyparser get ".env" | while read
       distribution='ubuntu-devel*'
     ;;
   esac
-  grep "${distribution}" .travis.yml | grep -v 'fail' | grep '>=2.4,<2.5' > /dev/null && v24=yes || v24=no
-  grep "${distribution}" .travis.yml | grep -v 'fail' | grep '>=2.5,<2.6' > /dev/null && v25=yes || v25=no
   grep "${distribution}" .travis.yml | grep -v 'fail' | grep '>=2.6,<2.7' > /dev/null && v26=yes || v26=no
-  grep "${distribution}" .travis.yml | grep -v 'fail' | grep '>=2.7,<2.8' > /dev/null && v27=yes || v27=no
+  grep "${distribution}" .travis.yml | grep -v 'fail' | grep 'version=\"\"' > /dev/null && v27=yes || v27=no
   grep "${distribution}" .travis.yml | grep -v 'fail' | grep 'version=\"devel\"' > /dev/null && devel='yes*' || devel='no*'
-  echo "|${distribution}|${v24}|${v25}|${v26}|${v27}|${devel}|"
+  echo "|${distribution}|${v26}|${v27}|${devel}|"
  done
 done | sort | uniq
 echo
