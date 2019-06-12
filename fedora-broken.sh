31770707b653c01e9949b2092a85d9e71e95ba8d
Robert de Bock <robert@meinit.nl>
2019-06-12 07:07:04 +0200

    Vagrant too.

--- fedora-broken.sh
+++ fedora-broken.sh
@@ -15,3 +15,11 @@ for scenario in default ; do
     sed -i "s%ansible-lint%ansible-lint\n  inventory:\n    host_vars:\n      ${rolename}-fedora-latest:\n        ansible_python_interpreter: /usr/bin/python3\n      ${rolename}-fedora-rawhide:\n        ansible_python_interpreter: /usr/bin/python3%" molecule/${scenario}/molecule.yml
   fi
 done
+
+for scenario in vagrant ; do
+  sed -i 's/29-cloud-base/30-cloud-base/' molecule/${scenario}/molecule.yml
+  grep "ansible_python_interpreter"  molecule/${scenario}/molecule.yml
+  if [ "$?" = "1" ] ; then
+    sed -i "s%ansible-lint%ansible-lint\n  inventory:\n    host_vars:\n      ${rolename}-fedora:\n        ansible_python_interpreter: /usr/bin/python3%" molecule/${scenario}/molecule.yml
+  fi
+done
