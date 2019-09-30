6e6cff4fe1b5e6333b3d0ab3224ee2b167fb3239
Robert de Bock <robert@meinit.nl>
2019-09-30 15:10:21 +0200

    Test if file exists before modifying it.

--- stdout_callback.sh
+++ stdout_callback.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
 for scenario in molecule/* ; do
-  grep 'stdout_callback: yaml' ${scenario}/molecule.yml > /dev/null 2>&1 || sed -i 's/scenario:/  config_options:\n    defaults:\n      stdout_callback: yaml\n      bin_ansible_callbacks: true\nscenario:/' ${scenario}/molecule.yml
+  if [ -f ${scenario}/molecule.yml} ] ; then
+    grep 'stdout_callback: yaml' ${scenario}/molecule.yml > /dev/null 2>&1 || sed -i 's/scenario:/  config_options:\n    defaults:\n      stdout_callback: yaml\n      bin_ansible_callbacks: true\nscenario:/' ${scenario}/molecule.yml
+  fi
 done
