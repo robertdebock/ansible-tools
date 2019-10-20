e4ea2a4a71e3913cca1c2d28298513f16ac6663d
Robert de Bock <robert@meinit.nl>
2019-10-20 17:55:59 +0200

    Cool tools.

--- tox.sh
+++ tox.sh
@@ -20,8 +20,10 @@ done
 cd ..
 }
 
+save_secret() {
 # Save the secret listed in .travis.yml
 secret=$(grep secure .travis.yml | cut -d\" -f2)
+}
 
 place_travis_yml() {
 # Place .travis.yml
@@ -35,28 +37,31 @@ echo "services:" >> .travis.yml
 echo "  - docker" >> .travis.yml
 echo "" >> .travis.yml
 echo "env:" >> .travis.yml
+echo "  global:" >> .travis.yml
+echo "    - namespace=\"robertdebock/\"" >> .travis.yml
 echo "  matrix:" >> .travis.yml
-echo "    - distribution=\"robertdebock/docker-alpine-openrc:latest\"" >> .travis.yml
-echo "    - distribution=\"robertdebock/docker-alpine-openrc:edge\"" >> .travis.yml
-echo "    - distribution=\"archlinux/base\"" >> .travis.yml
-echo "    - distribution=\"robertdebock/docker-centos-systemd:7\"" >> .travis.yml
-echo "    - distribution=\"robertdebock/docker-centos-systemd:latest\"" >> .travis.yml
-echo "    - distribution=\"robertdebock/docker-debian-systemd:latest\"" >> .travis.yml
-echo "    - distribution=\"robertdebock/docker-debian-systemd:stable\"" >> .travis.yml
-echo "    - distribution=\"robertdebock/docker-debian-systemd:unstable\"" >> .travis.yml
-echo "    - distribution=\"robertdebock/docker-fedora-systemd:latest\"" >> .travis.yml
-echo "    - distribution=\"robertdebock/docker-fedora-systemd:rawhide\"" >> .travis.yml
-echo "    - distribution=\"opensuse/leap\"" >> .travis.yml
-echo "    - distribution=\"robertdebock/docker-ubuntu-systemd:latest\"" >> .travis.yml
-echo "    - distribution=\"robertdebock/docker-ubuntu-systemd:devel\"" >> .travis.yml
-echo "    - distribution=\"robertdebock/docker-ubuntu-systemd:rolling\"" >> .travis.yml
+echo "    - image=\"docker-alpine-openrc\"" >> .travis.yml
+echo "    - image=\"docker-alpine-openrc\" tag=\"edge\"" >> .travis.yml
+echo "    # - namespace=\"archlinux/\" image=\"base\"" >> .travis.yml
+echo "    - image=\"docker-centos-systemd\" tag=\"7\"" >> .travis.yml
+echo "    - image=\"docker-centos-systemd\"" >> .travis.yml
+echo "    - image=\"docker-debian-systemd\" tag=\"stable\"" >> .travis.yml
+echo "    - image=\"docker-debian-systemd\" tag=\"unstable\"" >> .travis.yml
+echo "    - image=\"docker-debian-systemd\"" >> .travis.yml
+echo "    - image=\"docker-fedora-systemd\"" >> .travis.yml
+echo "    - image=\"docker-fedora-systemd\" tag=\"rawhide\"" >> .travis.yml
+echo "    - namespace=\"opensuse/\" image=\"leap\"" >> .travis.yml
+echo "    - image=\"docker-ubuntu-systemd\" tag=\"rolling\"" >> .travis.yml
+echo "    - image=\"docker-ubuntu-systemd\" tag=\"devel\"" >> .travis.yml
+echo "    - image=\"docker-ubuntu-systemd\"" >> .travis.yml
 echo "" >> .travis.yml
 echo "matrix:" >> .travis.yml
 echo "  allow_failures:" >> .travis.yml
-echo "    - env: distribution=\"alpine:edge\"" >> .travis.yml
-echo "    - env: distribution=\"robertdebock/docker-debian-systemd:unstable\"" >> .travis.yml
-echo "    - env: distribution=\"robertdebock/docker-fedora-systemd:rawhide\"" >> .travis.yml
-echo "    - env: distribution=\"robertdebock/docker-ubuntu-systemd:devel\"" >> .travis.yml
+echo "    - env: image=\"docker-alpine-openrc\" tag=\"edge\"" >> .travis.yml
+echo "    - env: image=\"docker-debian-systemd\" tag=\"unstable\"" >> .travis.yml
+echo "    - env: image=\"docker-fedora-systemd\" tag=\"rawhide\"" >> .travis.yml
+echo "    - env: image=\"docker-ubuntu-systemd\" tag=\"rolling\"" >> .travis.yml
+echo "    - env: image=\"docker-ubuntu-systemd\" tag=\"devel\"" >> .travis.yml
 echo "" >> .travis.yml
 echo "cache:" >> .travis.yml
 echo "  - pip" >> .travis.yml
@@ -87,8 +92,8 @@ echo "  name: yamllint" >> molecule/default/molecule.yml
 echo "driver:" >> molecule/default/molecule.yml
 echo "  name: docker" >> molecule/default/molecule.yml
 echo "platforms:" >> molecule/default/molecule.yml
-echo "  - name: \"\${distribution:-default}-\${TOX_ENVNAME:-default}\"" >> molecule/default/molecule.yml
-echo "    image: \"\${distribution:-robertdebock/docker-fedora-systemd:latest}\"" >> molecule/default/molecule.yml
+echo "  - name: \"\${image:-default}-\${TOX_ENVNAME:-default}\"" >> molecule/default/molecule.yml
+echo "    image: \"\${namespace}\${image:-docker-fedora-systemd}:\${tag:-latest}\"" >> molecule/default/molecule.yml
 echo "    command: sh -c \"while true ; do sleep 30 ; done\"" >> molecule/default/molecule.yml
 echo "    pre_build_image: yes" >> molecule/default/molecule.yml
 echo "provisioner:" >> molecule/default/molecule.yml
@@ -96,10 +101,6 @@ echo "  name: ansible" >> molecule/default/molecule.yml
 echo "  playbooks:" >> molecule/default/molecule.yml
 echo "    prepare: prepare.yml" >> molecule/default/molecule.yml
 echo "    converge: playbook.yml" >> molecule/default/molecule.yml
-echo "  inventory:" >> molecule/default/molecule.yml
-echo "    group_vars:" >> molecule/default/molecule.yml
-echo "      all:" >> molecule/default/molecule.yml
-echo "        ansible_python_interpreter: /usr/bin/python3" >> molecule/default/molecule.yml
 echo "scenario:" >> molecule/default/molecule.yml
 echo "  name: default" >> molecule/default/molecule.yml
 }
@@ -107,7 +108,7 @@ echo "  name: default" >> molecule/default/molecule.yml
 place_tox_ini() {
 echo "[tox]" > tox.ini
 echo "minversion = 3.7" >> tox.ini
-echo "envlist = py{37}-ansible-{previous,current,next}" >> tox.ini
+echo "envlist = py{37}-ansible-{current,next}" >> tox.ini
 echo "skipsdist = true" >> tox.ini
 echo "" >> tox.ini
 echo "[testenv]" >> tox.ini
@@ -122,7 +123,7 @@ echo "    molecule test" >> tox.ini
 echo "setenv =" >> tox.ini
 echo "    TOX_ENVNAME={envname}" >> tox.ini
 echo "    MOLECULE_EPHEMERAL_DIRECTORY=/tmp/{envname}" >> tox.ini
-echo "passenv = distribution" >> tox.ini
+echo "passenv = namespace image tag" >> tox.ini
 }
 
 add_tox_ini_to_git_ignore() {
@@ -131,6 +132,7 @@ grep .tox .gitignore 2> /dev/null 1>&2 || echo ".tox" >> .gitignore
 
 move_playbooks_to_default
 remove_other_scenarios
+save_secret
 place_travis_yml
 place_molecule_yml
 place_tox_ini
